9f3345ad17a0b59f826cb6703f78de0775ed1cf7
Rajat <you@example.com>
2018-04-22 08:07:59 +0530

    status added and updated

--- src/Ticket.js
+++ src/Ticket.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 import axios from "axios";
 import moment from "moment";
 import { v4 } from "uuid";
+import Select from "react-select";
+import "react-select/dist/react-select.css";
 
 export class Ticket extends Component {
   state = {
     tickets: null,
-    newTicket: false
+    newTicket: false,
+    status: ["pending", "resolved", "reopen"],
+    active: "pending",
+    selectedOption: ""
   };
 
   componentDidMount() {
@@ -23,23 +28,40 @@ export class Ticket extends Component {
   }
 
   handleBlur = e => {
-    let temp = this.state.tickets.find(ticket => ticket.id == e.target.name);
+    let isNewTicket = e.target.name == "issue" || e.target.name == "issue_by";
+    let temp = isNewTicket
+      ? this.state.tickets[this.state.tickets.length - 1]
+      : this.state.tickets.find(ticket => ticket.id == e.target.name);
     temp.created_at = moment(temp.created_at).format("YYYY-MM-DD HH:mm:ss");
-    axios
-      .patch("http://localhost:5050/ticket/", temp)
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+    !isNewTicket
+      ? axios
+          .patch("http://localhost:5050/ticket/", temp)
+          .then(response => {
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+          })
+      : !!temp.issue_by &&
+        !!temp.issue &&
+        axios
+          .post("http://localhost:5050/ticket/newticket/", temp)
+          .then(response => {
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+          });
   };
 
   handleIssue = e => {
-    let temp = !e.target.name
+    let isNewTicket = e.target.name == "issue" || e.target.name == "issue_by";
+    let temp = isNewTicket
       ? this.state.tickets[this.state.tickets.length - 1]
       : this.state.tickets.find(ticket => ticket.id == e.target.name);
-    temp.issue = e.target.value;
+    e.target.name == "issue_by"
+      ? (temp.issue_by = e.target.value)
+      : (temp.issue = e.target.value);
     temp.updated_at = moment().format("YYYY-MM-DD HH:mm:ss");
     this.setState(prevState => ({
       tickets: [...prevState.tickets, ...temp]
@@ -57,15 +79,39 @@ export class Ticket extends Component {
           {
             id: ++newTicketId,
             created_at: moment().format("YYYY-MM-DD HH:mm:ss"),
-            updated_at: moment().format("YYYY-MM-DD HH:mm:ss")
+            updated_at: moment().format("YYYY-MM-DD HH:mm:ss"),
+            status: "Pending"
           }
         ]
       ]
     }));
   };
 
+  handleChange = (id, selectedOption) => {
+    let newStatus = {id: id,status: selectedOption.value}
+    axios
+      .patch("http://localhost:5050/ticket", newStatus)
+      .then(response => {
+        console.log(response,"response")
+        this.setState(prevState => ({
+          tickets: [
+            ...prevState.tickets.slice(0, id - 1),
+            { ...prevState.tickets[id - 1], status: selectedOption.value },
+            ...prevState.tickets.slice(id)
+          ]
+        }));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
   render() {
-    const { tickets, newTicket } = this.state;
+    const { tickets, newTicket, status, selectedOption } = this.state;
+    let allCategories = [
+      { value: "Pending", label: "Pending" },
+      { value: "Resolved", label: "Resolved" },
+      { value: "Reopen", label: "Reopen" }
+    ];
     return (
       <div>
         <div className="card my-2 mx-4">
@@ -80,9 +126,15 @@ export class Ticket extends Component {
               <input
                 type="text"
                 className="form-control mx-2"
-                onChange={this.handleIssue.bind(this)}
+                onChange={this.handleIssue}
                 placeholder="Issue"
+                name="issue"
                 onBlur={this.handleBlur}
+                value={
+                  !tickets[tickets.length - 1].issue
+                    ? ""
+                    : tickets[tickets.length - 1].issue
+                }
               />
               <input
                 type="text"
@@ -90,6 +142,12 @@ export class Ticket extends Component {
                 onChange={this.handleIssue}
                 placeholder="Customer Name"
                 onBlur={this.handleBlur}
+                name="issue_by"
+                value={
+                  !tickets[tickets.length - 1].issue_by
+                    ? ""
+                    : tickets[tickets.length - 1].issue_by
+                }
               />
             </div>
           )}
@@ -102,8 +160,10 @@ export class Ticket extends Component {
               <th scope="col">Update At</th>
               <th scope="col">Issue</th>
               <th scope="col">Customer Name</th>
-              <th scope="col">Status</th>
               <th scope="col">Category</th>
+              <th scope="col" style={{ width: "15%" }}>
+                Status
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -136,8 +196,16 @@ export class Ticket extends Component {
                       />
                     </td>
                     <td>{issue_by}</td>
-                    <td>{status}</td>
                     <td>{category}</td>
+                    <td>
+                      <Select
+                        name="form-field-name"
+                        value={{ value: [id], label: tickets[id - 1].status }}
+                        onChange={this.handleChange.bind(this, id)}
+                        options={allCategories}
+                        clearable={false}
+                      />
+                    </td>
                   </tr>
                 )
               )}
